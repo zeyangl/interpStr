c6a84c1124b5ca8019428cfe0665b5b9b986adb0
Li Zeyang <a.banknote@gmail.com>
2015-09-06 18:37:51 +0800

    added reverse range

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@ int main()
     std::string path1 = "/Users/xuser/avatar[1-10].png";
     std::string path2 = "/Users/xuser/avatar[01-10].png";
     std::string path3 = "/Users/xuser/avatar[01-156,23, 13-20].png";
+    std::string path4 = "/Users/xuser/avatar[1-3,4,3-1, 1-1].png";
 
-    std::vector<std::string> stuff = interpStr(path3);
+    std::vector<std::string> stuff = interpStr(path4);
 
     for(auto& s : stuff) {
         std::cout << s << std::endl;
